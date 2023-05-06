2648590ed3d3d8f342bef02384e62c6152349e59
Gor <7495500@gmail.com>
2023-05-06 15:14:37 +0300

    Add projects to project

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,53 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[ show edit update destroy ]
+  before_action :is_admin!, except: %i[ index show ]
+
+  def index
+    @projects = Project.all
+  end
+
+  def show; end
+  def edit; end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to project_url(@project), notice: "Project was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to project_url(@project), notice: "Project was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_url, notice: "Project was successfully destroyed." }
+    end
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :link, :body)
+  end
+end
